530f14d1f5e44567321f90a1c2542d6331382400
Slava Shklyar <sshklyar@remedypartners.com>
2020-09-13 16:29:38 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # throt
 Golang IO throttling 
+
+Usage:
+```go
+package main
+ 
+ import "github.com/slavash/throt"
+ 
+ ...
+ 
+ fd, err := os.Open(fileName)
+ 
+ if err != nil {
+     return err
+ }
+ 
+ // set bandwidth limit per connection
+ connLimiter := rate.NewLimiter(rate.Limit(rateLimit), int(rateLimit))
+ // set bandwidth limit per server
+ globalLimiter := ctx.Value("rateLimit").(*rate.Limiter)
+ 
+ reader := throt.NewReader(ctx, fd)
+ reader.ApplyLimits(connLimiter, globalLimiter)
+ sent, err = io.Copy(c, reader)
+ 
+ ...
+ 
+ // The same may be done with writer:
+ 
+ writer := throt.NewWriter(ctx, c)
+ writer.ApplyLimits(connLimiter, globalLimiter)
+ sent, err = io.Copy(writer, fd)
+ 
+ sent, err = io.Copy(c, reader)
+
+ ...
+```
---
+++ writer.go
@@ -0,0 +1,47 @@
+package throt
+
+import (
+	"context"
+	"golang.org/x/time/rate"
+	"io"
+	"time"
+)
+
+type Writer struct {
+	ctx      context.Context
+	w        io.Writer
+	limiters []*rate.Limiter
+}
+
+// NewWriter create io.Writer implementation with rate limitation.
+func NewWriter(ctx context.Context, w io.Writer) *Writer {
+	return &Writer{
+		ctx: ctx,
+		w:   w,
+	}
+}
+
+// ApplyLimits set writing limit to bytePerSec bytes per second
+func (th *Writer) ApplyLimits(limiters ...*rate.Limiter) {
+	for _, l := range limiters {
+		l.AllowN(time.Now(), int(l.Limit())) // initialize a bucket with initial amount of tokens
+		th.limiters = append(th.limiters, l)
+	}
+}
+
+// Write writes bytes from p.
+func (th *Writer) Write(p []byte) (int, error) {
+	if th.limiters == nil {
+		return th.w.Write(p)
+	}
+	n, err := th.w.Write(p)
+	if err != nil {
+		return n, err
+	}
+	for _, l := range th.limiters {
+		if err := l.WaitN(th.ctx, n); err != nil {
+			return n, err
+		}
+	}
+	return n, err
+}
